b12807f490f649505aad6fd7b3973bd5853f9db2
Anindya Sharma <anindya.sharma1@gmail.com>
2021-03-02 13:58:16 +0000

    added age of family sequences

--- R/visualise.R
+++ R/visualise.R
@@ -29,6 +29,10 @@ plot_family_distances <- function(data) {
     dplyr::filter(step == 0) %>%
     dplyr::pull(raw) %>% nchar() %>% mean()
 
+  family_ages <- data$init %>%
+    dplyr::group_by(fam_tag) %>%
+    dplyr::summarise(fam_birth = min(real_time))
+
   to_plot <- data$pair %>%
     dplyr::inner_join(data$init %>%
                 dplyr::select(real_time, seq_tag, fam_tag, is_active) %>%
@@ -40,24 +44,35 @@ plot_family_distances <- function(data) {
               by = c('real_time', 'seq2')) %>%
     dplyr::mutate(same_family = fam1 == fam2) %>%
     dplyr::mutate(group_fam = paste(fam1, fam2, sep="_")) %>%
-    dplyr::mutate(seq_similarity = 1-dist/seq_length)
+    dplyr::mutate(seq_similarity = 1-dist/seq_length) %>%
+    dplyr::left_join(family_ages %>% dplyr::rename(fam1 = fam_tag, birth1 = fam_birth),
+                     by = 'fam1') %>%
+    dplyr::left_join(family_ages %>% dplyr::rename(fam2 = fam_tag, birth2 = fam_birth),
+                     by = 'fam2') %>%
+    dplyr::mutate(age1 = real_time - birth1) %>%
+    dplyr::mutate(age2 = real_time - birth2) %>%
+    dplyr::mutate(age = ifelse(age1 > age2, age1, age2))
 
   ggplot2::ggplot(to_plot) +
     ggplot2::geom_point(
       ggplot2::aes(x=real_time, y=seq_similarity,
                    group = group_fam,
                    #linetype=is_active,
-                   color=same_family
+                   color=same_family,
+                   size = age,
+                   alpha = 0.5
       )) +
   ggplot2::labs(x = "Time (millions of years)",
                 y = "Pairwise sequence similarity") +
-  ggplot2::scale_color_discrete(name = "Within same Family")
+  ggplot2::scale_color_discrete(name = "Within same Family") +
+  ggplot2::scale_size_continuous(name = "Age of Family (millions of years)") +
+  ggplot2::guides(alpha = FALSE)
 }
 
 #' Plot mosaic of recombination
 #' @param data Simulation data obtained using `input_data`
-#' @export
-plot_phylogeny <- function(data) {
+# TODO: plot a mosaic
+plot_phylogeny_mosaic <- function(data) {
   to_plot <- data$init %>%
     dplyr::select(-raw)
   starting <- to_plot %>%
@@ -79,5 +94,18 @@ plot_phylogeny <- function(data) {
     ggplot2::geom_line() +
     ggplot2::labs(x = "Time (millions of years)",
                   y = "Family ") +
-    ggplot2::scale_color_discrete(name = "Original family")
+    ggplot2::scale_color_discrete(name = "Original family") +
+    ggplot2::guides(color = FALSE)
+}
+
+
+#' Plot age of families
+#' @param data Simulation data obtained using `input_data`
+# TODO: plot a mosaic
+plot_phylogeny_mosaic <- function(data) {
+  to_plot <- data$init %>%
+    dplyr::select(-raw)
+  starting <- to_plot %>%
+    dplyr::select(step == 0) %>%
+    pull(seq_tag)
 }
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,10 +10,17 @@ int main() {
         max_total_copies;
     string point_mutation_model = "K80";
     double num_sensitive_positions, inactive_probability, burst_probability,
-           burst_mean, recomb_mean, selection_threshold, fam_proportion,
+           burst_mean, selection_threshold, fam_proportion,
            fam_percentage;
 
-    string simulation = "Phylogeny";
+    // recomb_means = 0, 1
+
+    int num_out_tags = 10;
+    int num_out_init = 10;
+    int num_out_seqs = 0;
+    int num_out_pair = 10;
+    string simulation = "Old";
+
     if (simulation == "Big") {
         num_seq = 10;
         seq_length = 5000;
@@ -29,8 +36,6 @@ int main() {
 
         max_active_copies = 100;
         max_total_copies =  1000;
-
-        recomb_mean = 0;
 
         selection_threshold = 10;
         fam_proportion = 0.7;
@@ -52,82 +57,101 @@ int main() {
         max_active_copies = 100;
         max_total_copies =  1000;
 
-        recomb_mean = 1;
-
         selection_threshold = 10;
         fam_proportion = 0.7;
         fam_percentage = 70;
     }
-    int num_out_tags = 10;
-    int num_out_init = 10;
-    int num_out_seqs = 0;
-    int num_out_pair = 10;
+    else if (simulation == "Old") {
+        num_seq = 10;
+        seq_length = 5000;
+
+        num_sensitive_positions = 10;
+        inactive_probability = 0.01;
+
+        num_jumps = 100;
+        timestep = 5;
+
+        burst_probability = 0.3;
+        burst_mean = 1;
+
+        max_active_copies = 100;
+        max_total_copies =  1000;
+
+        selection_threshold = 10;
+        fam_proportion = 0.7;
+        fam_percentage = 70;
+
+        num_out_init = 100;
+        num_out_pair = 10;
+    }
 
     bool to_randomise = false;
     bool to_seed = true;
     int seed = 0;
 
-    string filename_without = "main_simulation_without_flags_" +
-        to_string(num_seq) + "_" + to_string(seq_length) + "_" +
-        point_mutation_model + "_" +
-        to_string(num_jumps) + "_" + to_string(timestep) + "_" +
-        to_string(burst_probability) + "_" + to_string(burst_mean) + "_" +
-        to_string(max_active_copies) + "_" +
-        to_string(recomb_mean) + "_" +
-        to_string(num_out_tags) + "_" + to_string(num_out_init) + "_" +
-        to_string(num_out_seqs) + "_" + to_string(num_out_pair) + "_" +
-        to_string(to_randomise) + "_" + to_string(to_seed) + "_" +
-        to_string(seed) +
-        ".out";
-    string filename_with = "main_simulation_with_flags_" +
-        to_string(num_seq) + "_" + to_string(seq_length) + "_" +
-        point_mutation_model + "_" +
-        to_string(num_sensitive_positions) + "_" +
-        to_string(inactive_probability) + "_" +
-        to_string(num_jumps) + "_" + to_string(timestep) + "_" +
-        to_string(burst_probability) + "_" + to_string(burst_mean) + "_" +
-        to_string(max_active_copies) + "_" + to_string(max_total_copies) + "_" +
-        to_string(recomb_mean) + "_" +
-        to_string(selection_threshold) + "_" +
-        to_string(fam_proportion) + "_" + to_string(fam_percentage) + "_" +
-        to_string(num_out_tags) + "_" + to_string(num_out_init) + "_" +
-        to_string(num_out_seqs) + "_" + to_string(num_out_pair) + "_" +
-        to_string(to_randomise) + "_" + to_string(to_seed) + "_" +
-        to_string(seed) +
-        ".out";
+    for(int recomb_mean : {0}) {
+        string filename_without = "main_simulation_without_flags_" +
+            to_string(num_seq) + "_" + to_string(seq_length) + "_" +
+            point_mutation_model + "_" +
+            to_string(num_jumps) + "_" + to_string(timestep) + "_" +
+            to_string(burst_probability) + "_" + to_string(burst_mean) + "_" +
+            to_string(max_active_copies) + "_" +
+            to_string(recomb_mean) + "_" +
+            to_string(num_out_tags) + "_" + to_string(num_out_init) + "_" +
+            to_string(num_out_seqs) + "_" + to_string(num_out_pair) + "_" +
+            to_string(to_randomise) + "_" + to_string(to_seed) + "_" +
+            to_string(seed) +
+            ".out";
+        string filename_with = "main_simulation_with_flags_" +
+            to_string(num_seq) + "_" + to_string(seq_length) + "_" +
+            point_mutation_model + "_" +
+            to_string(num_sensitive_positions) + "_" +
+            to_string(inactive_probability) + "_" +
+            to_string(num_jumps) + "_" + to_string(timestep) + "_" +
+            to_string(burst_probability) + "_" + to_string(burst_mean) + "_" +
+            to_string(max_active_copies) + "_" + to_string(max_total_copies) + "_" +
+            to_string(recomb_mean) + "_" +
+            to_string(selection_threshold) + "_" +
+            to_string(fam_proportion) + "_" + to_string(fam_percentage) + "_" +
+            to_string(num_out_tags) + "_" + to_string(num_out_init) + "_" +
+            to_string(num_out_seqs) + "_" + to_string(num_out_pair) + "_" +
+            to_string(to_randomise) + "_" + to_string(to_seed) + "_" +
+            to_string(seed) +
+            ".out";
 
-    try
-    {
-        simulate_without_flags(
-            num_seq, seq_length, point_mutation_model,
-            num_jumps, timestep,
-            burst_probability, burst_mean,
-            max_active_copies,
-            recomb_mean,
-            filename_without,
-            num_out_tags, num_out_init, num_out_seqs, num_out_pair,
-            to_randomise, to_seed, seed,
-            0, 0, true
-        );
-        simulate_with_flags(
-            num_seq, seq_length,
-            point_mutation_model,
-            num_sensitive_positions, inactive_probability,
-            num_jumps, timestep,
-            burst_probability, burst_mean,
-            max_active_copies, max_total_copies,
-            recomb_mean,
-            selection_threshold, fam_proportion, fam_percentage,
-            filename_with,
-            num_out_tags, num_out_init, num_out_seqs, num_out_pair,
-            to_randomise, to_seed, seed,
-            0, 0, true
-        );
-    }
-    catch (Exception e)
-    {
-        std::cout << e.what() << std::endl;
-        return 1;
+        try
+        {
+            simulate_without_flags(
+                num_seq, seq_length, point_mutation_model,
+                num_jumps, timestep,
+                burst_probability, burst_mean,
+                max_active_copies,
+                recomb_mean,
+                filename_without,
+                num_out_tags, num_out_init, num_out_seqs, num_out_pair,
+                to_randomise, to_seed, seed,
+                0, 0, true
+            );
+            simulate_with_flags(
+                num_seq, seq_length,
+                point_mutation_model,
+                num_sensitive_positions, inactive_probability,
+                num_jumps, timestep,
+                burst_probability, burst_mean,
+                max_active_copies, max_total_copies,
+                recomb_mean,
+                selection_threshold, fam_proportion, fam_percentage,
+                filename_with,
+                num_out_tags, num_out_init, num_out_seqs, num_out_pair,
+                to_randomise, to_seed, seed,
+                0, 0, true
+            );
+        }
+        catch (Exception e)
+        {
+            std::cout << e.what() << std::endl;
+            return 1;
+        }
     }
     return 0;
 }
